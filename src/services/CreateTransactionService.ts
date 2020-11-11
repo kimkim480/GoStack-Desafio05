ef8ed24010c472c7318b2329f19be3dfa3ab5173
Eric <lordkim480@gmail.com>
2020-11-11 15:51:55 -0300

    Desafio 05

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,37 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import TransacionsReporitory from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransacionsReporitory);
+
+    const transactionBalance = transactionRepository.getBalance();
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
